907b7c0ee958d1f1dd49707165e73a6b1e14870f
Sofia <sofia.cronstrom@gmail.com>
2024-08-21 16:50:16 +0200

    adding useState to handle todos in todo list

--- src/Component/to-do.tsx
+++ src/Component/to-do.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
+interface item{
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
 export default function TodoList() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    (event.currentTarget as HTMLFormElement).reset();
-  };
+ const [todos, setTodos] = useState<item[]>([
+    {id: 1, text: "Städa", completed: false},
+    {id: 1, text: "Koda", completed: false}
+ ])
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
+    <section className="todo-container">
+        <ul className="todo-items">
+            {todos.map((todo) => (
+              <li key={todo.id}>{todo.text}</li>
+            ))}
+            
+        </ul>
+    <form className="form">
       <label htmlFor="todo">
         <input type="text" name="todo" id="todo" placeholder="Write a new task" />
       </label>
       <button className="submit-btn">Submit</button>
     </form>
+    </section>
   );
 }
